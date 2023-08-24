c24a34714d84d90152b3d9df1f8eb652b413e3b8
hwi215 <buc06137@naver.com>
2023-08-24 16:44:49 +0900

    add: @RequestParam(~~, value = "프론트에서 넘어오는 값") 으로 유지보수 높이기

--- spring/src/main/java/com/example/spring/controller/ParameterController.java
+++ spring/src/main/java/com/example/spring/controller/ParameterController.java
@@ -1,8 +1,11 @@
 package com.example.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller // 생성
 @RequestMapping("/param")
@@ -34,13 +37,36 @@ public class ParameterController {
     /**
      * void는 요청주소가 뷰이름이 된다
      * ex) param/b.do 이면 /WEB-INF/views/params/b.jsp
+     *
+     * 리턴이 void이거나, String일때 view정보만 전달되는데,
+     * Model정보도 함께 전달하고 싶을 때 Model을 인수로 받는다.
      */
-    @RequestMapping("/b.do")
-    public void bbb(@RequestParam(defaultValue = "Guest") String id,
-                    @RequestParam(defaultValue = "0") int age){ // 들어온 값이 null이면 기본값 설정
+
+    /*
+    @RequestMapping("/c.do")
+    public String cc(
+            @RequestParam(defaultValue = "Guest", value = "userId") String id){ // value = "프론트에서 넘어오는 id값"
         System.out.println("id = " + id);
-        System.out.println("age = " + age);
+
+        return "param/b";
     }
+
+     */
+
+    @RequestMapping("/c.do")
+    public String cc2( // value = "프론트에서 넘어오는 id값"
+            @RequestParam(defaultValue = "Guest", value = "userId") String id, Model model){
+        System.out.println("id = " + id);
+
+        // model에 정보 저장하기
+        model.addAttribute("message", "Spring MVC");
+        model.addAttribute("menu", Arrays.asList("짜장", "짬뽕", "탕수육"));
+
+
+
+        return "param/b";
+    }
+
 
 
 }
--- spring/src/main/webapp/WEB-INF/views/param/b.jsp
+++ spring/src/main/webapp/WEB-INF/views/param/b.jsp
@@ -6,11 +6,26 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
+
+<h2>param/b.do 요청완료</h2>
+
+메시지 : ${message} <p>
+ 메뉴 : ${menus}<br>
+
+
+<fieldset>
+    <legend> 메뉴 선택</legend>
+
+    <c:forEach items="${menu}" var = "menu">
+        <input type = "checkbox" name = "menu" value = "${menu}}">${menu}
+    </c:forEach>
+</fieldset>
 
 </body>
 </html>
