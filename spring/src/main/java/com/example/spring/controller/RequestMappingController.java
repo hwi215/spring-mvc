2329bf86941b41be225d7f3999411bdb5134747c
hwi215 <buc06137@naver.com>
2023-08-24 15:35:02 +0900

    add: @RequestMapping 예제

--- spring/src/main/java/com/example/spring/controller/RequestMappingController.java
+++ spring/src/main/java/com/example/spring/controller/RequestMappingController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller // 생성
+@RequestMapping("/rem")
 public class RequestMappingController {
 
     @RequestMapping("/a.do")
@@ -18,6 +21,31 @@ public class RequestMappingController {
         mv.addObject("message", "spring 배우기.");
         mv.setViewName("result"); // prefix + 뷰의 이름 + suffix
         return mv;
+
+    }
+
+    /**
+     * 여러개의 요청이 하나의 메소드를 호출해줌
+     *
+     * @return : String인 경우는, 리턴된 문자열이 뷰의 이름이 된다.
+     */
+    @RequestMapping(value = {"/b.do", "/c.do"})
+    public String bb(){
+        System.out.println("b.do와 c.do 요청됨....");
+        return "result"; // WEB-INF/views/result.jsp
+    }
+
+    /**
+     * @return : void인 경우는, 요청된 URL 주소가 뷰 이름이 된다.
+     * ex) rem/test.do 요청시 뷰의 이름은 rem/test가 된다.
+     * -> WEB-INF/views/rem/test.jsp
+     */
+    @RequestMapping("/test.do")
+    public void test(HttpServletRequest request){
+        System.out.println("rem/test.do 요청됨...");
+
+        String name = request.getParameter("name");
+        System.out.println("name: " + name);
 
 
     }
