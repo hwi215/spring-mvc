14297a50cc079ee1e7d37844a8d414c254194f8d
hwi215 <buc06137@naver.com>
2023-08-25 15:10:46 +0900

    add: @PathVariable 적용하기 - URI값에 가변형 변수를 전달받아 파라미터 처리

--- spring/src/main/java/com/example/spring/controller/PathVariableController.java
+++ spring/src/main/java/com/example/spring/controller/PathVariableController.java
@@ -2,14 +2,25 @@ package com.example.spring.controller;
 
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PathVariableController {
 
-    @RequestMapping("/user/jang.do")
-    public void test(){
+    @RequestMapping("/{type}/{id}.do")
+    public void test(@PathVariable String type, @PathVariable String id){  // 요청을 해로 잘 받기
         System.out.println("PathVariableController test() call...");
+
+        System.out.println("type: " + type);
+        System.out.println("id: " + id);
+    }
+
+    @RequestMapping("/jang")
+    public String test2(){
+
+        return "result";
+
     }
 
 
